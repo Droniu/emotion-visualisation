6b996a9f7a5a61ecb0e4247e8526f7c935e8a639
Damian Raczkowski <idontknow5325@gmail.com>
2023-12-02 13:37:05 +0100

    ADd umap, change invocation to single objects for pca and umap

--- api/api/dataset.py
+++ api/api/dataset.py
@@ -1,6 +1,6 @@
 import numpy as np
 from enum import Enum
-
+from umap import UMAP
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, normalize
 
@@ -31,5 +31,23 @@ class Dataset:
         return self.__pca.transform(features)
 
     def retrieve_train_set(self):
-        # print(self.__dataframe.head())
+        return self.__reduced_dim, self.__dataframe["best_label"].values, self.__dataframe['summary_y'].values
+
+
+class UMapImpl:
+
+    def __init__(self, dataframe, num_dim=2) -> None:
+        self.__dataframe = dataframe
+        self.__umap = UMAP(n_components=num_dim)
+        self.__scaler = StandardScaler()
+        train_features = np.vstack(self.__dataframe.features.values)
+        train_features = normalize(train_features)
+        scaled_features = self.__scaler.fit_transform(train_features)
+        self.__reduced_dim = self.__umap.fit_transform(scaled_features)
+
+
+    def count_umap_for_point(self, features):
+        return self.__umap.transform(features)
+
+    def retrieve_train_set(self):
         return self.__reduced_dim, self.__dataframe["best_label"].values, self.__dataframe['summary_y'].values
--- api/api/hello.py
+++ api/api/hello.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .models import EmotionModelWrapper, EmotionTokenizerWrapper
 from .api_models import Text
-from .dataset import Dataset, NumDimm
+from .dataset import Dataset, NumDimm, UMapImpl
 import asyncio
 from typing import Union
 import os
@@ -18,8 +18,8 @@ train_set = pd.read_pickle(__SAVE_DIR)
 app = FastAPI()
 tokenizer = EmotionTokenizerWrapper()
 model = EmotionModelWrapper()
-pca_2d = Dataset(train_set, num_dim=2)
-pca_3d = Dataset(train_set, num_dim=3)
+pca = Dataset(train_set, num_dim=3)
+umap = UMapImpl(train_set, num_dim=3)
 
 origins = [
     "http://localhost:4200",
@@ -45,21 +45,35 @@ async def main_route():
 @app.get("/pca")
 async def get_pca_data(size: Union[int, None] = None, dimensions: Union[NumDimm, NumDimm] = NumDimm.TWO):
 
-    if dimensions == NumDimm.TWO:
-        data_dimensions, labels, texts = pca_2d.retrieve_train_set()
-
-    elif dimensions == NumDimm.THREE:
-        data_dimensions, labels, texts = pca_3d.retrieve_train_set()
-
-    else:
-        raise Exception()
+    data_dimensions, labels, texts = pca.retrieve_train_set()
 
     if size is not None:
         data_dimensions = data_dimensions[:size, :]
         labels = labels[:size]
         texts = texts[:size]
 
-    converted_dimensions = data_dimensions.tolist()
+    converted_dimensions = data_dimensions[:, :int(dimensions.value)].tolist()
+    converted_labels = labels.tolist()
+    converted_texts = texts.tolist()
+
+    return {
+        "points": converted_dimensions,
+        "points_labels": converted_labels,
+        "points_texts": converted_texts
+    }
+
+
+@app.get("/umap")
+async def get_umap_data(size: Union[int, None] = None, dimensions: Union[NumDimm, NumDimm] = NumDimm.TWO):
+
+    data_dimensions, labels, texts = umap.retrieve_train_set()
+
+    if size is not None:
+        data_dimensions = data_dimensions[:size, :]
+        labels = labels[:size]
+        texts = texts[:size]
+
+    converted_dimensions = data_dimensions[:, :int(dimensions.value)].tolist()
     converted_labels = labels.tolist()
     converted_texts = texts.tolist()
 
@@ -76,12 +90,14 @@ async def send_input(text: Text):
     tokens = tokenizer.tokenize(text.text)
     model_task = loop.run_in_executor(None, model.produce_emotions, tokens)
     features, logits, labels = await model_task
-    points = pca_2d.count_pca_for_point(features).tolist()
+    points_pca = pca.count_pca_for_point(features).tolist()
+    points_umap = umap.count_umap_for_point(features).tolist()
 
     object_to_return = {
         # "logits": logits,
         "labels": labels,
-        "points": points,
+        "points_pca": points_pca,
+        "points_umap": points_umap,
         "input": text.text
     }
     saved_inputs.append(object_to_return)
@@ -89,5 +105,16 @@ async def send_input(text: Text):
 
 
 @app.get("/savedText")
-async def retrieve_saved_emotions():
-    return saved_inputs
+async def retrieve_saved_emotions(dimensions: Union[NumDimm, NumDimm] = NumDimm.TWO):
+
+    list_to_return = [
+        {
+            "labels": obj['labels'],
+            "points_pca": obj['points_pca'][0][:int(dimensions.value)],
+            "points_umap": obj['points_umap'][0][:int(dimensions.value)],
+            "input": obj['input']
+        }
+        for obj in saved_inputs
+    ]
+
+    return list_to_return
