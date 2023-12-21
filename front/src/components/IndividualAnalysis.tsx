c5565002ab3769d20c6fff48e6400abaa15dee07
droniu <droniu@droniu.dev>
2023-12-21 12:58:26 +0100

    Improve colors

--- front/src/components/IndividualAnalysis.tsx
+++ front/src/components/IndividualAnalysis.tsx
@@ -12,6 +12,7 @@ import {
 import { DataContext } from '../app/app';
 import themeColors from 'daisyui/src/theming/themes';
 import { useLocalStorage } from '@uidotdev/usehooks';
+import { COLORS } from '../consts';
 
 export const IndividualAnalysis = () => {
   const ctx = React.useContext(DataContext);
@@ -29,8 +30,8 @@ export const IndividualAnalysis = () => {
   );
 
   return (
-    <ResponsiveContainer width="100%" height="80%">
-      <BarChart data={rechartsData}>
+    <ResponsiveContainer width="100%" height="85%">
+      <BarChart data={rechartsData} margin={{ bottom: 40 }}>
         <CartesianGrid
           strokeDasharray="3 3"
           vertical={false}
@@ -45,13 +46,17 @@ export const IndividualAnalysis = () => {
         />
         <XAxis
           dataKey="name"
-          tick={{
-            fontSize: 9,
-            fill: themeColors[`[data-theme=${theme as 'dark' | 'light'}]`][
-              'base-content'
-            ],
-            fontFamily: 'sans-serif',
-          }}
+          // tick={{
+          //   fontSize: 13,
+          //   fill: themeColors[`[data-theme=${theme as 'dark' | 'light'}]`][
+          //     'base-content'
+          //   ],
+          //   fontFamily: 'sans-serif',
+          //   transform: 'scaleX(10)',
+          // }}
+          tick={<CustomTick />}
+          angle={30}
+          minTickGap={0}
         />
         <YAxis />
         <Tooltip
@@ -62,7 +67,7 @@ export const IndividualAnalysis = () => {
             fillOpacity: 0.5,
           }}
         />
-        <Legend />
+        <Legend wrapperStyle={{ bottom: '0%' }} />
         <Bar
           dataKey="score"
           fill={
@@ -71,5 +76,23 @@ export const IndividualAnalysis = () => {
         />
       </BarChart>
     </ResponsiveContainer>
+  );
+};
+
+const CustomTick = ({ x, y, payload }: any) => {
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text
+        x={0}
+        y={0}
+        dy={16}
+        textAnchor="end"
+        fontSize={12}
+        fill={COLORS[payload.value as keyof typeof COLORS]}
+        transform="rotate(-35)"
+      >
+        {payload.value}
+      </text>
+    </g>
   );
 };
